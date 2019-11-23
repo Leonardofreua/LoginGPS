fe99229f06f03acc3386cb7bdf6e102e7408ea18
LEONARDO BATISTA RODRIGUES FREUA <ALU201514123@uniritter.local>
2019-11-22 21:25:43 -0300

    added login screen

--- app/src/main/java/com/logingps/logingps/LoginActivity.java
+++ app/src/main/java/com/logingps/logingps/LoginActivity.java
@@ -1,14 +1,15 @@
 package com.logingps.logingps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
+        setTitle("Login");
     }
 }
