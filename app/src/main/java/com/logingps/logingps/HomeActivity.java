f552ded2708406d1dde449663dbf8e3b77b3f81f
leonardofreua <leonardo.batista.freua@gmail.com>
2019-11-22 21:50:00 -0300

    added home activity.

---
+++ app/src/main/java/com/logingps/logingps/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.logingps.logingps;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_activity);
+        setTitle("Home");
+    }
+
+    public void getCoordinates(View view) {
+
+    }
+
+    public void showReport(View view) {
+
+    }
+}
--- app/src/main/java/com/logingps/logingps/LoginActivity.java
+++ app/src/main/java/com/logingps/logingps/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.logingps.logingps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +14,10 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login_activity);
         setTitle("Login");
     }
+
+    public void signin(View view) {
+        Intent intent = new Intent(view.getContext(), HomeActivity.class);
+        startActivity(intent);
+    }
+
 }
