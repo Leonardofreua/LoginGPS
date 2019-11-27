786681e0e682ae4b11e6893412a9596d71c1de75
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2019-11-27 17:48:51 -0300

    finished user login

--- app/src/main/java/com/logingps/logingps/HomeActivity.java
+++ app/src/main/java/com/logingps/logingps/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.logingps.logingps;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
--- app/src/main/java/com/logingps/logingps/LoginActivity.java
+++ app/src/main/java/com/logingps/logingps/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.logingps.logingps;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.location.LocationRequest;
@@ -21,14 +27,21 @@ public class LoginActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
-
     private LocationRequest mLocationRequest;
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         setTitle("Login");
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
 
         createDefaultUser();
     }
@@ -53,9 +66,85 @@ public class LoginActivity extends AppCompatActivity {
                 });
     }
 
-    public void signin(View view) {
-        Intent intent = new Intent(view.getContext(), HomeActivity.class);
-        startActivity(intent);
+    public void signIn(final View view) {
+        String strEmail = email.getText().toString();
+        String strPassword = password.getText().toString();
+
+        Log.d("SingIn", "signIn:" + strEmail);
+        if (validateField(strEmail, strPassword)) {
+            mAuth.signInWithEmailAndPassword(strEmail, strPassword)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Log.d("SingIn", "signInWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                updateUI(view, user);
+                            } else {
+                                Log.w("SingIn", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(view.getContext(), "Login failed!", Toast.LENGTH_SHORT).show();
+                            }
+                            if (!task.isSuccessful()) {
+                                Log.w("SingIn", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(view.getContext(), "Login failed!", Toast.LENGTH_SHORT).show();
+                            }
+                            hideProgressDialog();
+                        }
+                    });
+        } else {
+            alertDialog();
+        }
     }
 
+    private void updateUI(View view, FirebaseUser user) {
+        hideProgressDialog();
+        if (user != null) {
+            Intent intent = new Intent(view.getContext(), HomeActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(view.getContext(), "Login failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    /**
+     * Valida se os campos de login foram preenchidos
+     *
+     * @return true se tudo estiver preenchido
+     * false se algum campo estiver vazio
+     */
+    private boolean validateField(String email, String password) {
+        if (email.isEmpty()) {
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Popup de alerta. Utilizada quando algum campo do formulário não estiver preenchido
+     */
+    private void alertDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Required Fields")
+                .setMessage("Verify that you filled in all fields")
+
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+    }
 }
