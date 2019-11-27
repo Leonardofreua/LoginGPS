6b102bdb78640eeb8398c3a81a0f6c8dd43c8317
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2019-11-27 16:59:28 -0300

    finished method to create default user

--- app/src/main/java/com/logingps/logingps/LoginActivity.java
+++ app/src/main/java/com/logingps/logingps/LoginActivity.java
@@ -2,17 +2,55 @@ package com.logingps.logingps;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+
+    private LocationRequest mLocationRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         setTitle("Login");
+
+        createDefaultUser();
+    }
+
+    private void createDefaultUser() {
+        mAuth.createUserWithEmailAndPassword("test@gmail.com", "123456")
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.i("CreateUser", "I'm here!");
+
+
+                        if (task.isSuccessful()) {
+                            Log.i("CreateUser", "Successfully registered User!");
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            Log.i("CreateUser", "Erro registered User!");
+                        }
+                    }
+                });
     }
 
     public void signin(View view) {
